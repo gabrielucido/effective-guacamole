cec95fb5e359adaea101ae94262ed80b102f60a5
gabrielucido <gabrielucido@gmail.com>
2023-10-03 17:49:43 -0400

    Interaction prefab v0

--- Assets/Scripts/InteractionController.cs
+++ Assets/Scripts/InteractionController.cs
@@ -2,30 +2,64 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
 
 public class InteractionController : MonoBehaviour
 {
-    private Vector3 _offset = new Vector3(0, 1.2f, 0);
-    public GameObject _objectToMove;
+    [SerializeField] private Sprite overlaySprite;
+    [SerializeField] private Vector3 offset = new Vector3(0, 1.2f, 0);
+
+    private bool _isInteractable = false;
+    private GameObject _imageObject;
+    private GameObject _overlayCanvas;
+    private Camera _mainCamera;
+
+    private void Start()
+    {
+        if (overlaySprite == null)
+        {
+            throw new NotImplementedException("Interactable Objects must have a overlay sprite set!");
+        }
+
+        if (Camera.main == null)
+        {
+            throw new NotImplementedException("Main Camera not found!");
+        }
+
+        _mainCamera = Camera.main;
+        _overlayCanvas = GetComponentInChildren<Canvas>().gameObject;
+        _imageObject = _overlayCanvas.gameObject.GetComponentInChildren<Image>().gameObject;
+        _imageObject.GetComponent<Image>().sprite = overlaySprite;
+        _overlayCanvas.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (_isInteractable)
+        {
+            _imageObject.transform.position = GetUiActionHintPosition();
+            if (Input.GetButtonDown("Interact"))
+            {
+                Debug.Log("Interaction!!!");
+            }
+        }
+    }
 
     private void OnTriggerExit2D(Collider2D other)
     {
-        _objectToMove.SetActive(false);
-    }
-
-    private void OnTriggerStay2D(Collider2D other)
-    {
-        _objectToMove.transform.position = GetUiActionHintPosition();
+        _overlayCanvas.SetActive(false);
+        _isInteractable = false;
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        _objectToMove.SetActive(true);
-        _objectToMove.transform.position = GetUiActionHintPosition();
+        _overlayCanvas.SetActive(true);
+        _isInteractable = true;
     }
 
     private Vector3 GetUiActionHintPosition()
     {
-        return Camera.main.WorldToScreenPoint(transform.position + _offset);
+        return _mainCamera.WorldToScreenPoint(transform.position + offset);
     }
 }
