6257f06f5a4732e0c5c1dddf3874342dc963cf0c
Lucas Muzza <67117275+lucasmuzza@users.noreply.github.com>
2023-10-06 03:04:56 -0400

    Added Menu Manager

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("LucasTestScene");
+    }
+
+    public void OpenOptions()
+    {
+
+    }
+
+    public void CloseOptions()
+    {
+
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Saiu");
+    }
+
+}
